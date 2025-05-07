f063e34ab74b716924764da155b3ba6e56b44d09
Southierzao <eng.pedrohsouthier@gmail.com>
2025-05-07 13:07:28 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+
+export default function Home() {
+  return (
+    <div className="bg-white text-zinc-900 font-sans">
+      <h1 className="text-3xl font-bold text-center py-20">VELLARE DIGITAL</h1>
+    </div>
+  );
+}
